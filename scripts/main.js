f802df5cda10519832d111ca452962ea43fc185e
StevenYin233 <zyin043@uottawa.ca>
2022-05-13 14:35:47 +0800

    chatbox_draft

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
+let quote = document.getElementById("quote");
+let button = document.getElementById("button");
+const url = "https://api.quotable.io/random";
+let getQuote = () => {
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      quote.innerText = item.content;
+    });
+};
+window.addEventListener("load", getQuote);
+button.addEventListener("click", getQuote);
+
 function time(){
     var timestamp = Date.now();     
     var tmp = new Date(timestamp);

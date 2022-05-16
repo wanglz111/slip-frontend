d14d786ec8d77d1ca73ddbd3a3fa8def218279d1
StevenYin233 <zyin043@uottawa.ca>
2022-05-17 02:46:56 +0800

    revise_information

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 let quote = document.getElementById("quote");
 let button = document.getElementById("button");
 const url = "https://api.quotable.io/random";
+
+var password=document.getElementById("password");
+
+
 let getQuote = () => {
   fetch(url)
     .then((data) => data.json())
@@ -15,5 +19,18 @@ function time(){
     var timestamp = Date.now();     
     var tmp = new Date(timestamp);
     document.getElementById("time").innerHTML = tmp;
+
+}
+function random_generator(){
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var passwordLength = 12;
+  var password = "";
+for (var i = 0; i <= passwordLength; i++) {
+ var randomNumber = Math.floor(Math.random() * chars.length);
+ password += chars.substring(randomNumber, randomNumber +1);
+}
+      document.getElementById("password").value = password
+      document.getElementById("confirmPassword").value = password
+
 
 }
